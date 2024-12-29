1c8c8ae6f38b695be451ebe025c138bf1ca5a872
blcoussama <belcadioussama.eng@gmail.com>
2024-12-29 13:32:59 +0100

    Set Up React Query Client and DevTools

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import AppLayout from './components/AppLayout'
 import { ThemeProvider } from './context/ThemeProvider'
 import WeatherDashboard from './routes/WeatherDashboard'
 import CityPage from './routes/CityPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={ <WeatherDashboard /> } />
-            <Route path="/city/:cityName" element={ <CityPage /> } />
-          </Routes>
-        </AppLayout>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <AppLayout>
+            <Routes>
+              <Route path="/" element={ <WeatherDashboard /> } />
+              <Route path="/city/:cityName" element={ <CityPage /> } />
+            </Routes>
+          </AppLayout>
+        </ThemeProvider>
       </BrowserRouter>
-    </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    
   )
 }
 
--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -9,7 +9,7 @@ const AppLayout = ({ children }: PropsWithChildren) => {
             {children}
         </main>  
         <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
-            <div className='container mx-auto px-4 text-center text-gray-400'>
+            <div className='container mx-auto px-4 text-center text-white'>
                 <p>Weather App Made By Belcadi Oussama</p>
             </div>
         </footer>
